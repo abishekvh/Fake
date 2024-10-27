5b1412122027ed9b7474ffaf852e59139d2f51d3
Abishek <138581071+abishekvh@users.noreply.github.com>
2024-10-27 16:30:07 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-
-const app = express();
-const PORT = 3000;
-
-// Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public')); // Serve static files from the public directory
-
-// Login endpoint
-app.post('/login', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    // Log the input values to a file
-    fs.appendFile('log.txt', `Username: ${username}, Password: ${password}\n`, (err) => {
-        if (err) throw err;
-    });
-
-    // Send a response back to the client
-    res.send('Login information received');
-});
-
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
-
+fetch('/login', {
+    method: 'POST',
+    body: formData
+})
+.then(response => {
+    console.log(response); // Log the entire response object
+    if (!response.ok) {
+        throw new Error('Login failed');
+    }
+    return response.text();
+})
